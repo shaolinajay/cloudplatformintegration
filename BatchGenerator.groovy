982553028fc54994cb698ec2558d0ba18c7beedb
shaolinajay <88328724+shaolinajay@users.noreply.github.com>
2025-12-16 10:09:18 +0530

    Add files via upload

---
+++ BatchGenerator.groovy
@@ -0,0 +1,45 @@
+/* Refer the link below to learn more about the use cases of script.
+https://help.sap.com/viewer/368c481cd6954bdfa5d0435479fd4eaf/Cloud/en-US/148851bf8192412cba1f9d2c17f4bd25.html
+
+If you want to know more about the SCRIPT APIs, refer the link below
+https://help.sap.com/doc/a56f52e1a58e4e2bac7f7adbf45b2e26/Cloud/en-US/index.html */
+import com.sap.gateway.ip.core.customdev.util.Message;
+import groovy.json.*;
+def Message processData(Message message) {
+
+    /*To set or modify the body, you can use the following methods.
+    def body = message.getBody();
+    message.setBody(body + " Body is modified");
+
+    //To set or modify the headers, you can use the following methods.
+    def headers = message.getHeaders();
+    def value = headers.get("oldHeader");
+    message.setHeader("oldHeader", value + " modified");
+    message.setHeader("newHeader", "newHeader");
+
+    //To set or modify the properties, you can use the following methods.*/
+    message.setHeader("Content-Type", "multipart/mixed;boundary=batch");
+    message.setHeader("Accept", "multipart/mixed");
+    def headers = message.getHeaders();
+    def cookies = headers.get("set-cookie");
+    def properties = message.getProperties();
+    InputStream payload = properties.get('body') as InputStream
+    message.setHeader("cookie",String.join(";",cookies));
+    def Entity = properties.get("Entity");
+    def body = payload.getText("UTF-8")
+    def BatchPayload="--batch\r\nContent-Type: multipart/mixed;boundary=changeset_001\r\n\r\n"
+    def json = new JsonSlurper().parseText(body);
+    def rows = json.Root.row  // List<Map>
+    def i=1
+    rows.each { r ->
+    def compact = JsonOutput.prettyPrint(JsonOutput.toJson(r))
+    BatchPayload = BatchPayload+"--changeset_001\r\nContent-Type: application/http\r\nContent-Transfer-Encoding: binary\r\nContent-ID: ${i}\r\n\r\nPOST ${Entity} HTTP/1.1\r\nContent-Type: application/json;odata.metadata=minimal\r\n\r\n${compact}\r\n\r\n"
+    i++;
+
+    }
+    BatchPayload+= "--changeset_001--\r\n\r\n--batch--"
+    message.setBody(BatchPayload)
+
+    return message;
+}
+
